d784ef62526993554eaaa5c1a8131b81e7d6afa6
jerichomata <74013719+jerichomata@users.noreply.github.com>
2022-04-14 22:26:59 -0700

    updated counter and added time

--- tweets.py
+++ tweets.py
@@ -1,9 +1,12 @@
 import snscrape.modules.twitter as sntwitter
 import pandas as pd
+import time, sys
 
-query = ' vestibular migraines headaches since:2012-01-01 until:2022-04-10 lang:"en" '
+query = ' vestibular headache OR vestibular migraine OR vestibular OR migraine since:2000-01-01 until:2022-04-14 lang:"en" '
 tweets = []
-limit = 8000
+limit = sys.maxsize
+counter = 0
+start_time = time.time()
 
 for tweet in sntwitter.TwitterSearchScraper(query).get_items():
     # print(vars(tweet))
@@ -12,6 +15,13 @@ for tweet in sntwitter.TwitterSearchScraper(query).get_items():
         break
     else:
         tweets.append([tweet.date, tweet.id, tweet.user.username, tweet.content, tweet.url])
+        # can also ret outlinks, tcooutlinks, replycount, retweetcount, likecount..
+        # https://github.com/igorbrigadir/twitter-advanced-search
+    print(str(counter))
+    counter +=1
+
+print("Completed in %s seconds" % (time.time() - start_time))
 
 df = pd.DataFrame(tweets, columns=['Datetime', 'Tweet ID', 'Username', 'Content', 'URL'])
+
 df.to_csv('tweets_df.csv')
